7c30246f10ce033afc758cd0f78884ad6d224ff0
Lim Xing Kang Norman <frenoid@gmail.com>
2023-04-20 18:29:37 +0800

    Update app to watch path recusrively

--- README.md
+++ README.md
@@ -10,6 +10,9 @@ Simple as.
 Install requirements <br>
 `pip3 install -r`
 
+Set a recursive watch path - s3fileshipper watches this path
+`export RECURSIVE_WATCH_PATH=/tmp`
+
 Run the app <br>
 `python3 app/main.py`
 
@@ -18,7 +21,7 @@ Build the image <br>
 `docker build . -t s3fileshipper`
 
 Run the image in detached mode <br>
-`docker run -d --name fileshipper s3fileshipper`
+`docker run -d --name fileshipper --env-file ./env.list s3fileshipper`
 
 Observe the logs <br>
 `docker logs fileshipper`
--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from os import getenv
 import inotify.adapters
 import logging
 import sys
@@ -7,14 +8,26 @@ logging.basicConfig(stream=sys.stdout,
                     format="%(asctime)s %(message)s",
                     datefmt="%m/%d/%Y &I:%M:%S %p")
 
+
+
 def main():
     logging.info("Start inotify")
-    i = inotify.adapters.Inotify()
+    RECURSIVE_WATCH_PATH: str = getenv("RECURSIVE_WATCH_PATH", None)
 
-    logging.info("Watching/tmp")
-    i.add_watch("/tmp")
+    if RECURSIVE_WATCH_PATH is None:
+        logging.info("Watches are None")
+        logging.info("RECURSIVE_WATCH_PATH must be non-empty")
+        sys.exit(1)
+            
+
+    logging.info("Recursively watching: {}".format(RECURSIVE_WATCH_PATH))
+    i = inotify.adapters.InotifyTree(RECURSIVE_WATCH_PATH)
 
     with open("/tmp/test_file", "w"):
+        pass
+    with open("/tmp/norman", "w"):
+        pass
+    with open("/tmp/file1.txt", "w"):
         pass
 
     for event in i.event_gen(yield_nones=False):
