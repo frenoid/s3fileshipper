98bdda30d505db5b101c7b83dc7baac2e3ee1c53
Lim Xing Kang Norman <frenoid@gmail.com>
2023-04-20 18:53:27 +0800

    Add s3 recursive shipping logic

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from os import getenv
+import boto3
 import inotify.adapters
 import logging
 import sys
@@ -18,21 +19,37 @@ def main():
         logging.info("Watches are None")
         logging.info("RECURSIVE_WATCH_PATH must be non-empty")
         sys.exit(1)
-            
+
+    AWS_ACCESS_KEY_ID = getenv("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = getenv("AWS_SECRET_ACCESS_KEY")
+    S3_BUCKET = getenv("S3_BUCKET")
+    S3_KEY_PREFIX = getenv("S3_KEY_PREFIX")
+    S3_ENDPOINT = getenv("S3_ENDPOINT", None)
+    
+    s3_client = boto3.client("s3")
 
     logging.info("Recursively watching: {}".format(RECURSIVE_WATCH_PATH))
     i = inotify.adapters.InotifyTree(RECURSIVE_WATCH_PATH)
 
-    with open("/tmp/test_file", "w"):
+    with open("/tmp/test_file", "w") as f:
+        f.write("data data data")
         pass
-    with open("/tmp/norman", "w"):
+    with open("/tmp/norman", "w") as f:
+        f.write("data data data")
         pass
-    with open("/tmp/file1.txt", "w"):
+    with open("/tmp/file1.txt", "w") as f:
+        f.write("data data data")
         pass
 
     for event in i.event_gen(yield_nones=False):
         (_, type_names, path, filename) = event
         logging.info("PATH=[{}] FILENAME=[{}] EVENT_TYPES={}".format(path, filename, type_names))
+
+        if "IN_CLOSE_WRITE" in type_names:
+            logging.info("Uploading file {}/{}".format(path, filename))
+            reponse = s3_client.upload_file("{}/{}".format(path, filename),
+                                            S3_BUCKET,
+                                            "{}/{}".format(S3_KEY_PREFIX, filename))
 
 if __name__ == "__main__":
     main()
